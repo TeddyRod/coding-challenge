488c94f72e79bd192ac884ff262d755e2e10ab56
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-05-25 16:57:03 +0200

    Routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import NavMain from './components/NavMain';
-import Home from './ pages/Home';
+import Home from './pages/Home';
 import Temperature from './pages/Temperature';
 import CustomozeImage from './pages/CustomizeImage';
 import './App.css';
